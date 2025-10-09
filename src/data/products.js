f338c122b77bb21b8640d6f6ef3da00d0566857e
Dmizuta <dani.mizuta@gmail.com>
2025-10-09 17:50:47 -0300

    feat:categories

--- src/data/products.js
+++ src/data/products.js
@@ -3,27 +3,27 @@ export const products = {
     name: "T-Shirts",
     img: "/images/categories/tshirts.png",
     subcategories: [
-      { id: "a", name: "Style A", img: "/images/models/tshirts/a.png", colors: ["red","blue","black"] },
-      { id: "b", name: "Style B", img: "/images/models/tshirts/b.png", colors: ["white","navy"] },
-      { id: "c", name: "Style C", img: "/images/models/tshirts/c.png", colors: ["green","yellow"] },
-      { id: "d", name: "Style D", img: "/images/models/tshirts/d.png", colors: ["gray","red"] },
-      { id: "e", name: "Style E", img: "/images/models/tshirts/e.png", colors: ["black","orange"] },
+      { id: "a", name: "Style A", img: "/images/models/tshirts/a.png", colors: ["red","blue","yellow"] },
+      { id: "b", name: "Style B", img: "/images/models/tshirts/b.png", colors: ["red","blue","yellow"] },
+     // { id: "c", name: "Style C", img: "/images/models/tshirts/c.png", colors: ["green","yellow"] },
+     // { id: "d", name: "Style D", img: "/images/models/tshirts/d.png", colors: ["gray","red"] },
+     // { id: "e", name: "Style E", img: "/images/models/tshirts/e.png", colors: ["black","orange"] },
     ]
   },
   shorts: {
     name: "Shorts",
     img: "/images/categories/shorts.png",
     subcategories: [
-      { id: "a", name: "Short A", img: "/images/models/shorts/a.png", colors: ["blue","black"] },
-      { id: "b", name: "Short B", img: "/images/models/shorts/b.png", colors: ["gray","white"] },
+      { id: "a", name: "Short A", img: "/images/models/shorts/a.png", colors: ["red","blue","yellow"] },
+      { id: "b", name: "Short B", img: "/images/models/shorts/b.png", colors: ["red","blue","yellow"] },
     ]
   },
   hoodies: {
     name: "Hoodies",
     img: "/images/categories/hoodies.png",
     subcategories: [
-      { id: "a", name: "Hoodie A", img: "/images/models/hoodies/a.png", colors: ["black","white","green"] },
-      { id: "b", name: "Hoodie B", img: "/images/models/hoodies/b.png", colors: ["red","gray"] },
+      { id: "a", name: "Hoodie A", img: "/images/models/hoodies/a.png", colors: ["red","blue","yellow"] },
+      { id: "b", name: "Hoodie B", img: "/images/models/hoodies/b.png", colors: ["red","blue","yellow"] },
     ]
   }
 };
