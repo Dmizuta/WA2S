2380763c7e364c8648f6d70e7a9709340c273a81
Dani Mizuta <dani.mizuta@gmail.com>
2025-10-10 20:31:13 -0300

    feat

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "./HeaderFooter.css";
 export default function Footer() {
   return (
     <footer className="site-footer">
-      <p>© 2025 A2 Sports – Todos os direitos reservados ⚡</p>
+      <p>© 2025 2A Sports – Todos os direitos reservados ⚡</p>
     </footer>
   );
 }
