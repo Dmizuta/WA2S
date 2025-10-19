4f37168207377594ef147c0d11ccc27bbc3dbd40
Dani Mizuta <dani.mizuta@gmail.com>
2025-10-19 00:25:04 -0300

    feat: refactor images folders

--- src/pages/MonteFabric.jsx
+++ src/pages/MonteFabric.jsx
@@ -1,10 +1,18 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import CanvasEditor from "../components/CanvasEditor.jsx";
 
 export default function MonteFabric() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-2xl font-bold mb-6">Monte o Seu Uniforme</h1>
-      <CanvasEditor />
-    </div>
+    <>
+      <Header />
+
+      <main className="min-h-screen flex flex-col items-center justify-center bg-gray-100 py-10 px-4">
+        <h1 className="text-2xl font-bold mb-6">Monte o Seu Uniforme</h1>
+        <CanvasEditor />
+      </main>
+
+      <Footer />
+    </>
   );
 }
